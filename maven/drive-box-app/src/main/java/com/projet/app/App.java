1acf1b76f4a8ad59c89ebf9ae3d8351b1b830553
ilyass <hp@hp-PC>
2016-05-10 15:48:26 +0200

    Ajout maven

---
+++ maven/drive-box-app/src/main/java/com/projet/app/App.java
@@ -0,0 +1,13 @@
+package com.projet.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
