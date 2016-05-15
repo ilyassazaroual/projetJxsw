3d13be5d10c10625957cfc2bcf8f66dd5ed0b2cb
gael35 <gael-herbrecht35@hotmail.fr>
2016-05-16 00:53:31 +0200

    authorize dropbox google

---
+++ src/main/java/net/our/api/App.java
@@ -0,0 +1,11 @@
+package net.our.api;
+import java.io.*;
+
+public class App
+{
+    public static void main(String[] args){
+	//	Client client = ClientBuilder.newClient();
+	//WebTarget target = client.target("http://localhost:9998").path("resource");
+    }
+
+}
