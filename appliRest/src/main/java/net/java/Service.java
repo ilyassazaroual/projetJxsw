1313fa346866101ad500d302ea5c21bede1e6dcb
AliMohamed <AliMohamed@DESKTOP-9VQHM03>
2016-05-16 17:12:15 +0200

    authentification bdd

--- appliRest/src/main/java/net/java/Service.java
+++ appliRest/src/main/java/net/java/Service.java
@@ -1,15 +1,23 @@
 package net.java;
 
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
-import entites.User;
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+
+
+
+
 
 
 @Path("/service")
@@ -18,10 +26,23 @@ public class Service {
 	@POST
     @Path("/{auth}")
 	@Consumes("application/x-www-form-urlencoded")
-    public String authentification( @FormParam("log") String log,@FormParam("mp") String mp) {
+    public String authentification( @FormParam("log") String log,@FormParam("mp") String mp) throws SQLException {
         System.out.println("********************"+log);
-        if(log.equals("admin") && mp.equals("admin")) return "Authentification réeussie";
-        else return "<h3>erreur d'authentidfication</h3>";
+        String str="";
+        try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.print("pilote chargé");
+			Connection conect = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/projetjxsw","root","");
+			Statement st = (Statement) conect.createStatement();
+			String req = "select * from user where login=\""+log+"\" and password=\""+mp+"\"";
+			System.out.println("**********"+req);
+			ResultSet res=st.executeQuery(req);
+			if(res.first()) str= "OK";
+			else str = "erreur d'authentidfication";
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+        return str;
     }   
 	
 	
@@ -30,9 +51,9 @@ public class Service {
     @Path("/{id}")
     //@Produces(MediaType.APPLICATION_JSON)
     public String getHello( @PathParam("id") int id) {
-        User user = new User(id, "Ali", 23);
+        //User user = new User(id, "Ali", 23);
         if(id !=2) return "eureur";
-        else return user.getUsername();
+        else return "OK";
     } 
 
 }
