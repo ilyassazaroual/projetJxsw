1bacdd6e4683375f6f55c95f9373de9bf17f2067
gael35 <gael-herbrecht35@hotmail.fr>
2016-05-19 21:58:44 +0200

    rename and move for drive

--- src/main/java/net/our/api/MoveFiles.java
+++ src/main/java/net/our/api/MoveFiles.java
@@ -22,33 +22,27 @@ public class MoveFiles {
     @Path("/dropbox")
 	//mvntarget.queryParam("path",chemin);
     public Response MoveFileDropbox(@QueryParam("pathfrom") String pathfrom, @QueryParam("pathto") String pathto) {
-	Client client = ClientBuilder.newClient();
-	ClientRest clientrest=ClientRest.getinstance();
-
-	WebTarget target = client.target("https://api.dropboxapi.com/1/fileops/move"+"?root=auto&from_path="+pathfrom+"&to_path="+pathto);
-	Invocation.Builder invocationBuilder =  target.request(MediaType.APPLICATION_JSON);
-	invocationBuilder.header(HttpHeaders.AUTHORIZATION,"Bearer " + clientrest.getTokenDropbox());
-	Response response = invocationBuilder.get();
-	return response;
+		Client client = ClientBuilder.newClient();
+		ClientRest clientrest=ClientRest.getinstance();
+		WebTarget target = client.target("https://api.dropboxapi.com/1/fileops/move"+"?root=auto&from_path="+pathfrom+"&to_path="+pathto);
+		Invocation.Builder invocationBuilder =  target.request(MediaType.APPLICATION_JSON);
+		invocationBuilder.header(HttpHeaders.AUTHORIZATION,"Bearer " + clientrest.getTokenDropbox());
+		Response response = invocationBuilder.get();
+		return response;
     }
 
-    /******* TODDOO*****/
     @GET
     @Produces ("application/json")
-    @Path("/drive")
-    public Response MoveFileDrive(@QueryParam("path") String fileId) {
-
-    ClientRest clientrest=ClientRest.getinstance();
-
-	Client client = ClientBuilder.newClient();
-	WebTarget target = client.target("https://www.googleapis.com/drive/v2/files/").path(fileId);
-	Invocation.Builder invocationBuilder =  target.request(MediaType.APPLICATION_JSON);
-	invocationBuilder.header(HttpHeaders.AUTHORIZATION,"Bearer " + clientrest.getTokenDrive());
-	Response response = invocationBuilder.delete();
-	return response;
-	
-
-
+    @Path("/drive/{fileid}")
+    public Response MoveFileDrive(@PathParam("fileid") String file_id, @QueryParam("newfolderid") String foler_id) {
+		ClientRest clientrest=ClientRest.getinstance();
+		Client client = ClientBuilder.newClient();
+		String test="{\n \"parents\": [\n  {\n   \"id\": \""+foler_id+"\"\n  }\n ]\n}";
+		WebTarget target = client.target("https://www.googleapis.com/drive/v2/files/"+file_id+"?fields=parents");
+		Invocation.Builder invocationBuilder =  target.request(MediaType.APPLICATION_JSON);
+		invocationBuilder.header(HttpHeaders.AUTHORIZATION,"Bearer " + clientrest.getTokenDrive());
+		Response response = invocationBuilder.put(Entity.entity(test,MediaType.APPLICATION_JSON));
+		return response;	
     }
 
 }
